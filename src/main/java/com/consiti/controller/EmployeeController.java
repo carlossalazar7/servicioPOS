a953d375c7aac360f32a2caf83ab31be3db6353d
carlossalazar7 <carlossalazar2228@gmail.com>
2022-02-14 17:11:05 -0600

    cambios en controlador y entidad

--- src/main/java/com/consiti/controller/EmployeeController.java
+++ src/main/java/com/consiti/controller/EmployeeController.java
@@ -3,30 +3,17 @@ package com.consiti.controller;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 import com.consiti.entity.Employee;
 import com.consiti.repository.EmployeeRepository;
-//import com.consiti.repository.EmployeeRepository;
-//import com.consiti.service.IEmployeeService;
 import com.consiti.serviceImplement.EmployeeService;
-//import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
-@Controller
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 	
 	@Autowired
@@ -36,18 +23,13 @@ public class EmployeeController {
 	EmployeeRepository	repository;
 
 	
-	@GetMapping(value = "/employees", produces=  {"application/json"})
+	@GetMapping(value = "/", produces=  {"application/json"})
 	public @ResponseBody List<Employee> listar(){
 		return employeeservice.findAll();
 
 	}
-	
-	/**
-	 * Devuelve empleado por id
-	 * @param id
-	 * @return
-	 */
-	@GetMapping(value="/employee/{id}", produces=  {"application/json"})
+
+	@GetMapping(value="/{id}", produces=  {"application/json"})
 	public ResponseEntity<?> findById(@PathVariable("id") String id) {
 		try {
 			//Comprobar que el usuario existe por id
@@ -55,7 +37,6 @@ public class EmployeeController {
 			if (existe) {
 				//Si existe devuelve al empleado
 				Employee employee = employeeservice.findOne(id);
-				
 				return ResponseEntity.status(HttpStatus.OK).body(employee);
 			}
 		} catch (Exception e) {
@@ -64,12 +45,8 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Empleado con id "+id+" no encontrado");
 	}
 	
-	/**
-	 * Crea un nuevo empleado en la DB
-	 * @param newEmployee
-	 * @return
-	 */
-	@PostMapping(value="/employees",produces=  {"application/json"})
+
+	@PostMapping(value="/",produces=  {"application/json"})
 	public ResponseEntity<?> createEmployee(@RequestBody Employee newEmployee) {
 		
 		try {
@@ -82,13 +59,8 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
-	/**
-	 * Modifica los campos de un empleado por id
-	 * @param id
-	 * @param updatedEmployee
-	 * @return
-	 */
-	@PutMapping(value="/employee/{id}")
+
+	@PutMapping(value="/{id}")
 	public ResponseEntity<?> updateEmployee(@PathVariable("id") String id,@RequestBody Employee updatedEmployee) {
 		try {
 			boolean existe = repository.existsById(id);
@@ -108,12 +80,8 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 
-	/**
-	 * Elimina un empleado por id
-	 * @param id
-	 * @return
-	 */
-	@DeleteMapping(value="/employee/{id}", produces=  {"application/json"})
+
+	@DeleteMapping(value="/{id}", produces=  {"application/json"})
 	public ResponseEntity<?> deleteEmployee(@PathVariable("id") String id) {
 		
 		try {
--- src/main/java/com/consiti/entity/Employee.java
+++ src/main/java/com/consiti/entity/Employee.java
@@ -1,11 +1,11 @@
 package com.consiti.entity;
 
-//import lombok.Data;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-//@Data
+@Data
 @Entity
 @Table(name = "EMPLOYEE")
 public class Employee implements Serializable {
@@ -35,100 +35,5 @@ public class Employee implements Serializable {
 
     @Column(name = "EMP_TYPE", nullable = false, length = 20)
     private String empType;
-
-
-    public Employee() {
-    }
-
-
-    public Employee(String accountName, String userId, String phone, String name, String salesPerson, String cashierInfo, String empType) {
-        this.accountName = accountName;
-        this.userId = userId;
-        this.phone = phone;
-        this.name = name;
-        this.salesPerson = salesPerson;
-        this.cashierInfo = cashierInfo;
-        this.empType = empType;
-    }
-
-
-    public String getId() {
-        return this.id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getAccountName() {
-        return this.accountName;
-    }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public String getUserId() {
-        return this.userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSalesPerson() {
-        return this.salesPerson;
-    }
-
-    public void setSalesPerson(String salesPerson) {
-        this.salesPerson = salesPerson;
-    }
-
-    public String getCashierInfo() {
-        return this.cashierInfo;
-    }
-
-    public void setCashierInfo(String cashierInfo) {
-        this.cashierInfo = cashierInfo;
-    }
-
-    public String getEmpType() {
-        return this.empType;
-    }
-
-    public void setEmpType(String empType) {
-        this.empType = empType;
-    }
-
-
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", accountName='" + getAccountName() + "'" +
-            ", userId='" + getUserId() + "'" +
-            ", phone='" + getPhone() + "'" +
-            ", name='" + getName() + "'" +
-            ", salesPerson='" + getSalesPerson() + "'" +
-            ", cashierInfo='" + getCashierInfo() + "'" +
-            ", empType='" + getEmpType() + "'" +
-            "}";
-    }
 
 }
