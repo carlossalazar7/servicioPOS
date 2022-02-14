bc6593914ea9c25a1dd2a3cfdbd3c822a00a7e80
Revelos <cristianrevelo98@gmail.com>
2022-02-14 11:14:51 -0600

    Service  & ServiceImplement

--- src/main/java/com/consiti/controller/EmployeeController.java
+++ src/main/java/com/consiti/controller/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.consiti.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.consiti.entity.Employee;
+import com.consiti.service.IEmployeeService;
+
+@Controller
 public class EmployeeController {
+	
+	@Autowired
+	IEmployeeService employeeservice;
+	
+	@GetMapping(value = "/employees", produces=  {"application/json"})
+	public @ResponseBody List<Employee> listar(){
+		return employeeservice.findAll();
+	}
 }
--- src/main/java/com/consiti/service/IEmployeeService.java
+++ src/main/java/com/consiti/service/IEmployeeService.java
@@ -1,5 +1,19 @@
 package com.consiti.service;
 
+import java.util.List;
+
+import com.consiti.entity.Employee;
+
+
+
 public interface IEmployeeService {
 
+	public List<Employee> findAll();
+
+	public void save(Employee empleado);
+
+	public Employee findOne(Integer id);
+
+	public void delete(Integer id);
+	
 }
